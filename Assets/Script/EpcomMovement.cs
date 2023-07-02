e6cde59a8d715d69bba5c753596eb49c8da17670
aditd5 <aditdhiantono@gmail.com>
2023-07-02 13:09:36 +0700

    EDA
    initial commit

--- Assets/Script/EpcomMovement.cs
+++ Assets/Script/EpcomMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EpcomMovement : MonoBehaviour
 {
+
+    private Rigidbody2D body;
+    public float speed;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(hInput * speed, body.velocity.y);
     }
 }
